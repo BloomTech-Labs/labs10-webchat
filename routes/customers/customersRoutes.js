a33af51b80658899562fef48bff593cba5a9d02e
Linda Yang <lindayang@Lindas-iMac.fios-router.home>
2019-02-13 23:27:17 -0500

    Implemented get and getById for customersRoutes

--- routes/customers/customersRoutes.js
+++ routes/customers/customersRoutes.js
@@ -4,8 +4,32 @@ const db = require('../../data/helpers/customersDb');
 
 
 router.get('/', (req, res) => {
-      res.status(200).send('Hello Customers....');
+	db.get()
+		.then(customers => {
+			res.status(200).json(customers);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		})
 });
+
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	const request = db.getById(id);      
+	request.then(res => { 
+		console.log(res);
+
+		if(res.length == 0) {
+			res.status(404).json({ error: "The user with the specified Id does not exist" });
+		} else {
+			console.log(res);
+			res.status(200).json(res);
+		}
+	})
+	.catch(err => {
+		res.status(500).json({ err: "Failed to retrieve the user" })
+	});
+})
 
 
 module.exports = router;
