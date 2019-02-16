ef908b4f3e53bdc03442bd802aa04a69e9ab7b86
Linda Yang <yanglinda24@gmail.com>
2019-02-16 01:41:57 -0500

    Implemented getById endpoint for companiesRoutes

--- routes/companies/companiesRoutes.js
+++ routes/companies/companiesRoutes.js
@@ -13,4 +13,23 @@ router.get('/', (req, res) => {
 		})
 });
 
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	const request = db.getById(id);
+
+	request.then(response_data => { 
+		console.log(response_data);
+
+		if(response_data.length == 0) {
+			res.status(400).json({ error: "The company with the specified id does not exist" });
+		} else {
+			console.log(response_data);
+			res.status(200).json(response_data);
+		}
+	})
+	.catch(err => {
+		res.status(500).json({ err: "Failed to retrieve company's details" });
+	});
+})
+
 module.exports = router;
