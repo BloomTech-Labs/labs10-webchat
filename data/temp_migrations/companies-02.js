c6e4b8a446e744677e154654977ab0d53ef23cbe
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-02-18 12:24:54 -0800

    added has_paid boolean to companies table temp migration file

---
+++ data/temp_migrations/companies-02.js
@@ -0,0 +1,21 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('companies', table => {
+            table.increments('id').primary();           // id of the company
+            table.string('name')
+                .unique()
+                .notNullable();
+            table.string('api_token')
+                .unique()
+                .notNullable();
+            table.boolean('has_paid')
+                .defaultTo(false);
+        })
+    ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfEsists('companies')
+    ])
+};
