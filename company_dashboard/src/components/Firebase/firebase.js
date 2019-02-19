1f47d8897b941a80ee1f2963af90735765f2797b
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-19 11:28:39 -0800

    Used the higher-order component to wrap CustomerSignUpForm.

--- company_dashboard/src/components/Firebase/firebase.js
+++ company_dashboard/src/components/Firebase/firebase.js
@@ -2,7 +2,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
