efc1d394011a72449d607b7aec33ccc945495ff3
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-19 11:31:22 -0800

    restored to initial setup

--- company_dashboard/src/App.js
+++ company_dashboard/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 
 import LandingPage from './components/LandingPage';
+//import CustomerSignUp from './components/Customer/CustomerSignUp';
+
 class App extends Component {
   render() {
     return (
