9f9d49159dfb02c5b84525a9f457888ea862f8fc
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-18 20:49:36 -0800

    created Firebase config file and initialized firebase with the configuration

---
+++ company_dashboard/src/components/Firebase/firebase.js
@@ -0,0 +1,20 @@
+import app from 'firebase/app';
+
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP__FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP__FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP__FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP__FIREBASE_MESSAGING_SENDER_ID,
+};
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
