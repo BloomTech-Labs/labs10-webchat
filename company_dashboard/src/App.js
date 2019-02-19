392ba56256399443f38ffa2f0059befa57facd3a
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-02-19 09:33:54 -0800

    Changes LandingPage to SettingsNavigation to test stripe.

--- company_dashboard/src/App.js
+++ company_dashboard/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import LandingPage from './components/LandingPage';
+// import LandingPage from './components/LandingPage';
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <LandingPage />
+//       </div>
+//     );
+//   }
+// }
+
+import SettingsNavigation from './components/settings/SettingsNavigation';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingPage />
+        <SettingsNavigation />
       </div>
     );
   }
