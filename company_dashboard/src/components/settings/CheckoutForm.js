d0c0814fa8f0ddca18bac9992993ab6ef8af4f39
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-02-19 11:12:38 -0800

    Added axios to checkout form charge post.

--- company_dashboard/src/components/settings/CheckoutForm.js
+++ company_dashboard/src/components/settings/CheckoutForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import axios from 'axios';
 
 class CheckoutForm extends Component {
   constructor(props) {
@@ -10,19 +11,29 @@ class CheckoutForm extends Component {
 
   async submit(ev) {
     let { token } = await this.props.stripe.createToken({ name: "Name" });
+    console.log("Stripe token from checkout form submit: ", token);
     let company_id = 1;
     // Company ID is hardcoded in until we have user accounts set up
 
-    let response = await fetch("/charge", {
-      method: "POST",
-      headers: { "Content-Type": "text/plain" },
-      body: {
-        "token": token.id,
-        "company_id": company_id
-      }
-    });
+    // let response = await fetch("http://localhost:5000/api/billing/charge", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "text/plain" },
+    //   body: {
+    //     "token": token.id,
+    //     "company_id": company_id
+    //   }
+    // });
+    let chargeRequest = { token: token.id, company_id: company_id};
+    axios.post("http://localhost:5000/api/billing/charge", chargeRequest)
+      .then(response => {
+        console.log("charge response: ", response);
+      })
+      .catch(err => {
+        console.log(err);
+      })
 
-    if (response.ok) this.setState({ complete: true });
+
+    // if (response.ok) this.setState({ complete: true });
   }
 
   render() {
