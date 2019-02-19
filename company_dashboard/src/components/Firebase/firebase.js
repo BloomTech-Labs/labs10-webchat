3076de1a076fe593018394648332b5446b4bfb0d
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-19 08:53:01 -0800

    Added Firebase authentication functions as class methods, they will communicate from the Firebase class to the Firebase API to register and login user credentials.

--- company_dashboard/src/components/Firebase/firebase.js
+++ company_dashboard/src/components/Firebase/firebase.js
@@ -2,7 +2,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -16,6 +16,14 @@ class Firebase {
     app.initializeApp(config);
     this.auth = app.auth();	  
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();	
 }
 
 export default Firebase;
