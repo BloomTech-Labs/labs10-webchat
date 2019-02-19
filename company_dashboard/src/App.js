7c3cba27319f4a79ea766c4c486af67d080ffa82
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-02-19 12:12:49 -0800

    Changed app.js to render landingPage

--- company_dashboard/src/App.js
+++ company_dashboard/src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 
-// import LandingPage from './components/LandingPage';
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <LandingPage />
-//       </div>
-//     );
-//   }
-// }
-
-import SettingsNavigation from './components/settings/SettingsNavigation';
+import LandingPage from './components/LandingPage';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SettingsNavigation />
+        <LandingPage />
       </div>
     );
   }
