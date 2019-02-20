a7c88ecce2fc002a1e9a1844b2fd90edd9bb65ca
verydecent <wnjhwng@gmail.com>
2019-02-20 10:34:27 -0800

    creates component wireframes for Customer Chat

---
+++ company_dashboard/src/components/chat/CustomerChat.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class CustomerChat extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            blank: ''
+        };
+
+    }
+
+    render() {
+        return (
+            <div>
+                TEXT
+            </div>
+        );
+    }
+};
+
+export default CustomerChat;
---
+++ company_dashboard/src/components/chat/CustomerWaiting.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class CustomerWaiting extends React {
+    constructor() {
+        super();
+
+        this.state = {
+            blank: ''
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                TEXT
+            </div>
+        )
+    }
+}
+
+export default CustomerWaiting;
