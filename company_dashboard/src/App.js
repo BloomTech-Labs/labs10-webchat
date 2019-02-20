559df4c93a3261d491b5af06ff43651d997539b2
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-19 22:19:22 -0800

    added Rep Register component to Routes

--- company_dashboard/src/App.js
+++ company_dashboard/src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
-//import { Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import LandingPage from './components/LandingPage';
 import CustomerSignUp from './components/Customer/CustomerSignUp';
 import RepsLogin from "./components/representatives/RepsLogin";
+import RepRegister from "./components/representatives/RepRegister"
+//import CompanyRegister from "./components/company/CompanyRegister";
 
 class App extends Component {
   render() {
     return (
        <Router>	    
        <div className="App">
-
-       <Route exact path={ROUTES.LANDING} component={LandingPage} />
-       <Route exact path={ROUTES.REPS_LOGIN} component={RepsLogin} /> 	    
-       <Route exact path={ROUTES.CUSTOMER_SIGN_UP} component={CustomerSignUp} />	
+	
+       <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+       <Route path={ROUTES.REPS_LOGIN} component={RepsLogin} /> 	    
+       <Route path={ROUTES.CUSTOMER_SIGN_UP} component={CustomerSignUp} />	
+       <Route path={ROUTES.REP_REGISTER} component={RepRegister} />	    
 	    
       </div>
       </Router>	    
