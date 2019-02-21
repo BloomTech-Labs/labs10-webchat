56c57ddf2b88b45cb26689bcb3803d5a928fc8b1
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-21 08:33:42 -0800

    importing all the required libraries

--- company_dashboard/src/components/representatives/RepsLogin.js
+++ company_dashboard/src/components/representatives/RepsLogin.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withFirebase } from "../Firebase";
+import { Link, withRouter, Route} from "react-router-dom"
+import { FirebaseContext } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 const RepLoginPage = () => (
   <div>
