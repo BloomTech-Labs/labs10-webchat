7a701384eb1ae0d3b56aa77ba9785b560d15cd38
Linda Yang <yanglinda24@gmail.com>
2019-02-20 19:52:54 -0500

    Modified AdminPanel routes

--- company_dashboard/src/constants/routes.js
+++ company_dashboard/src/constants/routes.js
@@ -4,4 +4,5 @@ export const SIGN_IN = '/signin';
 //export const HOME = '/home';
 export const CUSTOMER_SIGN_UP = '/customersignup';
 export const REPS_LOGIN = '/repslogin';
+export const ACCOUNT_SETTINGS = '/accountsettings';
 export const ADMIN_PANEL = '/adminpanel';
