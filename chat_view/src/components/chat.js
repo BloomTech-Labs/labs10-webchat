b3f8ee131bfff7d8ef4fa74c7e36cfbad2222caa
verydecent <wnjhwng@gmail.com>
2019-02-20 17:59:57 -0800

    Imports socket.io-client and Adjusts line spacing to 2 for Chat.js Component

--- chat_view/src/components/chat.js
+++ chat_view/src/components/chat.js
@@ -1,44 +1,49 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class Chat extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: '',
-            message: '',
-            messages: []
-        };
-    }
-    render() {
-        return(
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-4">
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="card-title">
-                                    Global Chat
-                                    </div>
-                                    <hr/>
-                                    <div className="messages">
-
-                                    </div>
-                                    <div className="footer">
-                                        <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
-                                        <br/>
-                                        <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
-                                        <br/>
-                                        <button className="btn btn-primary form-control">Send</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	constructor() {
+		super();
+		this.state = {
+			username: '',
+			message: '',
+			messages: []
+		};
+	}
+	render() {
+		return(
+			<div>
+				<div className="container">
+					<div className="row">
+						<div className="col-4">
+							<div className="card">
+								<div className="card-body">
+									<div className="card-title">
+									Global Chat
+									</div>
+									<hr/>
+									<div className="messages">
+										{this.state.messages.map((messages, index) => {
+											return(
+												<div>{message.author} : {message.message}</div>
+											);
+										})}
+									</div>
+									<div className="footer">
+										<input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
+										<br/>
+										<input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+										<br/>
+										<button className="btn btn-primary form-control">Send</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Chat;
