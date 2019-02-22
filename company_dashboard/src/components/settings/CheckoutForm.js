a133e152017a29b272df40fa17c496d663ae9116
chainchompa <chainchompa@gmail.com>
2019-02-21 19:03:49 -0500

    changed url method in checkoutform

--- company_dashboard/src/components/settings/CheckoutForm.js
+++ company_dashboard/src/components/settings/CheckoutForm.js
@@ -17,10 +17,9 @@ class CheckoutForm extends Component {
     console.log("Stripe token from checkout form submit: ", token);
     let company_id = 1;
     // Company ID is hardcoded in until we have user accounts set up
-    const url = `${process.env.REACT_APP_BACKEND_URL}billing/charge/`;
     let chargeRequest = { token: token.id, company_id: company_id };
     axios
-      .post(`${url}`, chargeRequest)
+      .post("/api/billing/charge", chargeRequest)
       .then(response => {
         console.log("charge response: ", response);
         this.setState({ complete: true });
