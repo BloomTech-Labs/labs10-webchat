de04e7aa6292f333f8de60411ba77579c3476847
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-23 22:24:14 -0800

    Created GET and getById endpoint for approved emails table

--- routes/approvedemails/approvedemails.js
+++ routes/approvedemails/approvedemails.js
@@ -4,6 +4,37 @@ const db = require('../../data/helpers/approvedemailDb');
 
 
 
+router.get('/', (req, res) => {
+        	const request = db.get();
+                
+		request.then(approvedemails => {
+                        res.status(200).json(approvedemails);
+                })
+                .catch(err => {
+                        res.status(500).json({err: err.message});
+                })
+});
+
+router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        const request = db.getById(id);
+
+        request.then(response_data => { 
+                console.log(response_data);
+
+                if(response_data.length == 0) {
+                        res.status(400).json({ error: "The user  with the specified id does not exist" });
+                } else {
+                        console.log(response_data);
+                        res.status(200).json(response_data);
+                }
+        })
+        .catch(err => {
+                res.status(500).json({ err: err.message });
+        });
+})
+
+
 router.delete('/:id', (req, res) => {
         const {id} = req.params;
 
