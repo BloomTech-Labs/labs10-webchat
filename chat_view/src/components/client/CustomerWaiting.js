31b16891c1b97516db73296314f9da1730eaa26b
verydecent <wnjhwng@gmail.com>
2019-02-26 12:04:18 -0800

    clients

---
+++ chat_view/src/components/client/CustomerWaiting.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class CustomerWaiting extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            blank: ''
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                TEXT
+            </div>
+        )
+    }
+}
+
+export default CustomerWaiting;
---
+++ chat_view/src/components/client/Dummy.js
@@ -0,0 +1,17 @@
+import React from "react";
+import io from "socket.io-client";
+
+console.log(io);
+
+class Dummy extends React.Component {
+
+  render() {
+    return(
+      <div>
+        TEST        
+      </div>
+
+    );
+  }
+}
+export default Dummy;
