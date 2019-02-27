1b99e2a5ffd53b2ad14a8cf08b35ea127ab79b58
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-02-26 18:41:57 -0800

    add conversations table migration file created but not migrated.

---
+++ data/migrations/20190226134303_add-conversations-table.js
@@ -0,0 +1,24 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('conversations', table => {
+            table.increments('id').primary();       
+            table.integer('company_id')
+                .references('id')
+                .inTable('companies');
+            table.string('rep_uid');     // will be nullable to start because rep will not be assigned when convo is created
+            table.string('customer_uid') // will aslo serve as socket room name
+                .references('uid')
+                .inTable('customers');
+            table.boolean('in_q')
+                defaultsTo(true);
+            table.boolean('is_open')
+                defaultsTo(true);
+        })
+    ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfEsists('conversations')
+    ])
+};
