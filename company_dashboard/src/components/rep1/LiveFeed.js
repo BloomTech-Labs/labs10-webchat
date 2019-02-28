ab8095b4b79fb55a8a425e72a6e04783bd99835d
Sukhada Gholba <sukhadagholba@gmail.com>
2019-02-27 18:30:16 -0800

    Added firebase auth to authenticate before getting customer questions on represenattaive dashboard

--- company_dashboard/src/components/rep1/LiveFeed.js
+++ company_dashboard/src/components/rep1/LiveFeed.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import socket from 'socket.io-client';
+import io from 'socket.io-client';
+import { withFirebase } from "../Firebase";
+import { Link, withRouter, Route} from "react-router-dom"
+import { FirebaseContext } from '../Firebase';
 import Query from './Query';
 import QueryPanel from './QueryPanel';
 import ChatRepPage from './ChatRepPage';
@@ -7,38 +10,57 @@ import './Query.css';
 import axios from 'axios';
 
 
-class LiveFeed extends Component {
+
+const LiveFeedPage = () => (
+  <div>
+    <FirebaseContext.Consumer>
+      {firebase => <LiveFeedComponent firebase={firebase} />}
+    </FirebaseContext.Consumer>
+  </div>
+);
+
+
+
+class LiveFeedFormBase extends Component {
   constructor(props) {
     super(props)
     this.state = {
       rep_id: props.history.location.state.rep_id,	    
       clickedQuery: false,
       currentQuery: null,
+      error: null,	    
       queries: [1, 2, 3],
     }
-    
-    socket.on("send_data", function(data) {
-      console.log("send_data");
-      //let queries = this.state.queries;
-      //queries.push(data);
-      //this.setState({ queries });
-      // or this.setState({ queries: [...this.state.queries, data] })
-    });
+   
+	  
   }
-  
+
   componentDidMount() {
-  const id = this.props.history.location.state.rep_id;	  
-    const request = axios.get('/api/customers/company/:id');
-        	
-	       request.then(response => {
-			console.log('query: ', response.data);
+    	
+	this.props.firebase.auth.currentUser.getIdToken()
+          .then(idToken => {
+            console.log("idToken in livefeed page: ", idToken);
+            
+	    axios.defaults.headers.common['Authorization'] = idToken; 
+
+	    console.log('rep_is is', this.props.history.location.state.rep_id);	
+	     const id = this.props.history.location.state.rep_id;	  
+	     const request = axios.get(`/api/customers/company/${id}`);
+
+                request.then(response => {
+                        console.log('query: ', response.data);
                 })
-                .catch(error =>{
-                        console.log(error.message);
-                })	  
-  }
-  
-  
+                .catch(err =>{
+                        console.log(err.message);
+                })
+
+	  })
+	  .catch(error => {
+	    console.log(error.message);	  
+            this.setState({ error:error });
+          })
+	  
+}  
 
   openQuery = (query) => {
     this.setState({ clickedQuery: !this.state.clickedQuery });
@@ -68,4 +90,10 @@ class LiveFeed extends Component {
   }
 }
 
-export default LiveFeed;
+const LiveFeedComponent = withRouter(withFirebase(LiveFeedFormBase));
+
+export default LiveFeedPage;
+
+export {LiveFeedComponent};
+
+
