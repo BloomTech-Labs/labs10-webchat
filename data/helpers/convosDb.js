e8b0cb904052d2859f3931dc48029296e419a2e0
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-03-03 18:56:23 -0800

    getByUid helper created to get conversations in the queue based on rep's uid.

---
+++ data/helpers/convosDb.js
@@ -0,0 +1,21 @@
+const db = require('../db.js');
+
+module.exports = {
+    getByRepUid
+}
+
+// Get conversation info to poppulate Queue using signed-in rep's uid
+// * Potential To-Do: get customer's name to display for the rep
+function getByRepUid() {
+    const query = db
+        .select([
+            "representatives.name as rep_name",
+            "representatives.company_id as rep_company_id",
+            "conversations.customer_uid",
+            "conversations.summary",
+        ])
+        .from("representatives")
+        .innerJoin("conversations", "representatives.company_id", "conversations.company_id")
+        .where("representatives.uid", uid)
+        .where("conversations.in_q", true;
+}
--- routes/chat/index.js
+++ routes/chat/index.js
@@ -3,3 +3,7 @@ const router = express.Router();
 const convosDb = require('../../data/helpers/convosDb');
 const messagesDb = require('../../data/helpers/messagesDb');
 
+// Get all conversations in queue using uid of signed-in rep:
+router.get('/conversations' (req, res) => {
+    convosDb
+})
