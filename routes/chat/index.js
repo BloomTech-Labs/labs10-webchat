9a22158dbb17e98b202562b39f236022333f5222
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-03-04 16:44:45 -0800

    getActive endpoint created to get active convos.

--- routes/chat/index.js
+++ routes/chat/index.js
@@ -16,6 +16,7 @@ router.get('/queue', (req, res) => {
         })
 })
 
+// Remove conversation from Queue by changing in_q to false
 router.put('/dequeue', (req, res) => {
     const id = req.body.id;
     console.log("id in /dequeue endpoint: ", id);
@@ -29,6 +30,18 @@ router.put('/dequeue', (req, res) => {
         })
 })
 
+// Get all active conversations
+router.get('/active', (req, res) => {
+    const uid  = req.body.uid;      // uid should come from server auth sequence based on rep's idToken
+    const request = convosDb.getActive(uid);
+    request
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(error => {
+            res.status(500).json({ message: error.message });
+        })
+})
 
 
 module.exports = router;
