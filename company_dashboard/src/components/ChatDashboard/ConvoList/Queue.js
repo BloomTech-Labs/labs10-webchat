2ffbbaed9526e140ecffe3eb4ed318dbabb8a102
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-03-05 12:04:42 -0800

    Queue query/conversation element styling updated.

--- company_dashboard/src/components/ChatDashboard/ConvoList/Queue.js
+++ company_dashboard/src/components/ChatDashboard/ConvoList/Queue.js
@@ -1,6 +1,27 @@
-import React from 'react';
-import { Link, withRouter} from "react-router-dom"
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+import { withRouter} from "react-router-dom"
+import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom'
 import axios from 'axios';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+      overflow: 'hidden',
+      padding: `0 ${theme.spacing.unit * 3}px`,
+    },
+    paper: {
+      maxWidth: 400,
+      margin: `${theme.spacing.unit}px auto`,
+      padding: theme.spacing.unit * 2,
+    },
+});
 
 class Queue extends React.Component {
     constructor(props) {
@@ -26,26 +47,60 @@ class Queue extends React.Component {
 
 
     render() {
-        return (
-            <div>
-                {this.state.conversations.map((convo, index) => {
-                    return (
+        const { classes } = this.props;
+    return (
+      <div>
+        <MuiThemeProvider>    
+          <Typography color='inherit' variant='h4' align='center'>Message Queue</Typography><br/><br/>     
+            {this.state.conversations.map((convo, index) => {
+              return(
+                <Paper key={index} className={classes.paper}>
+                  <Grid container wrap="nowrap" spacing={16}>
+                    <Grid item>
+                    </Grid>
+                    <Grid item xs zeroMinWidth>
                         <div 
-                            className="convo-list-item" 
-                            key={index}
+                            key={index} 
                             onClick={() => this.props.handleQueueConvoSelect(convo.convo_id, convo.customer_uid, convo.summary)}
                         >
-                            <p>Customer: {convo.customer_name}</p>
-                            
-                            <p>Summary: {convo.summary}</p>
-
-                            <p>Convo ID: {convo.convo_id}</p>
+                        <Typography 
+                            color='primary' 
+                            variant='h5' 
+                            align='center' 
+                            noWrap 
+                            key={index}
+                        >
+                          Customer Question:{convo.summary}
+                        </Typography>
                         </div>
-                    )
-                })}
-            </div>
-        )
+                    </Grid>
+                  </Grid>
+                </Paper>
+              )	 
+            })}
+        </MuiThemeProvider>      
+      </div>
+    );
+        // return (
+        //     <div>
+        //         {this.state.conversations.map((convo, index) => {
+        //             return (
+        //                 <div 
+        //                     className="convo-list-item" 
+        //                     key={index}
+        //                     onClick={() => this.props.handleQueueConvoSelect(convo.convo_id, convo.customer_uid, convo.summary)}
+        //                 >
+        //                     <p>Customer: {convo.customer_name}</p>
+                            
+        //                     <p>Summary: {convo.summary}</p>
+
+        //                     <p>Convo ID: {convo.convo_id}</p>
+        //                 </div>
+        //             )
+        //         })}
+        //     </div>
+        // )
     }
 }
 
-export default withRouter(Queue);
+export default withStyles(styles)(withRouter(Queue));
