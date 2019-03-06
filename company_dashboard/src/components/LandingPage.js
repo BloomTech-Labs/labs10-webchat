8c13cc71f7145e83228778b47c84c60e13a038ae
Linda Yang <yanglinda24@gmail.com>
2019-03-06 00:26:29 -0500

    Deleted commented out codes

--- company_dashboard/src/components/LandingPage.js
+++ company_dashboard/src/components/LandingPage.js
@@ -285,32 +285,6 @@ class LandingPage extends Component {
             </Grid>
         </Grid>
       </div>
-      
-      
-      // <div className="landing-page">
-      //   <div className="navigation-container">
-      //     <Button size="large">
-      //       <Link to={ROUTES.REPS_LOGIN}>Sign In</Link>
-      //     </Button>
-      //     <Button size="large">
-      //       <Link to={ROUTES.REP_REGISTER}>Sign Up</Link>
-      //     </Button>
-      //     <img
-      //       className="netlify-logo"
-      //       src={require("./images/logomark.png")}
-      //       alt="Netlify logo"
-      //     />
-      //   </div>
-      //   <div className="landing-info">
-      //     <h1>Chattr</h1>
-      //     <p>
-      //       Welcome to Chattr, the new way to chat with your customers.
-      //     </p>
-      //     <Button variant="outlined" color="primary" className="upload-button">
-      //       Get Started
-      //     </Button>
-      //   </div>
-      // </div>
     );
   }
 }
