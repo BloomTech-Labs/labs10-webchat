3569c3f2e097eb6489bf252a58d7bfd04ca3c826
chainchompa <chainchompa@gmail.com>
2019-03-06 13:15:54 -0500

    created navigation for logged in users

--- company_dashboard/src/components/Navigation.js
+++ company_dashboard/src/components/Navigation.js
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 import SignOutButton from './SignOut';
 import * as ROUTES from '../constants/routes';
-
+import './Navigation.css'
 
 const Navigation = () => (
   <div className="navigation">
     <img src="https://i.ibb.co/Mpy1WhB/3029ba78-770c-49a3-aaa6-6a6cfc58b56c.png" alt="logo" />
-    <Link to={ROUTES.LANDING}>Chat Dashboard</Link>
-    <Link to={ROUTES.LANDING}>Account</Link>
-    <Link to={ROUTES.LANDING}>Log Out</Link>
+    <div className="navigation-links">
+      <Link to={ROUTES.CHAT_DASHBOARD}>Chat Dashboard</Link>
+      <Link to={ROUTES.ADMIN_SETTINGS}>Account</Link>
+      <Link to={ROUTES.LANDING}>Log Out</Link>
+    </div>
   </div>
 );
 
