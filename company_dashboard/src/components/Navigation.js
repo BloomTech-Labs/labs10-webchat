98432e6893add4305df5056204d7737d7ef0757e
chainchompa <chainchompa@gmail.com>
2019-03-06 13:36:53 -0500

    removed link to settings since it crashes atm

--- company_dashboard/src/components/Navigation.js
+++ company_dashboard/src/components/Navigation.js
@@ -10,7 +10,6 @@ const Navigation = () => (
     <img src="https://i.ibb.co/Mpy1WhB/3029ba78-770c-49a3-aaa6-6a6cfc58b56c.png" alt="logo" />
     <div className="navigation-links">
       <Link to={ROUTES.CHAT_DASHBOARD}>Chat Dashboard</Link>
-      <Link to={ROUTES.ADMIN_SETTINGS}>Account</Link>
       <Link to={ROUTES.LANDING}>Log Out</Link>
     </div>
   </div>
