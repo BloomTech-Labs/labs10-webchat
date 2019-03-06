7f703fec7cdcb0944f9b60e55236bef00c51237d
chainchompa <chainchompa@gmail.com>
2019-03-06 11:23:25 -0500

    fixing account settings

--- company_dashboard/src/components/settings/AccountSettings.js
+++ company_dashboard/src/components/settings/AccountSettings.js
@@ -79,8 +79,9 @@ class AccountSettings extends React.Component {
     };
     console.log(user);
 
-    axios
-      .put(`/api/reps/updaterepinfo`, user)
+    const request = axios.put('/api/reps/updaterepinfo', user);
+
+    request
       .then(response => {
         console.log("User info updated");
         console.log(response);
