e844fe4357e0f1a3fd49ae9d95cb06a2b40dd7f0
Sukhada Gholba <sukhadagholba@gmail.com>
2019-03-06 18:33:27 -0800

    Added heroku link to socket

--- company_dashboard/src/components/rep1/ChatRepPage.js
+++ company_dashboard/src/components/rep1/ChatRepPage.js
@@ -46,9 +46,8 @@ class ChatRepPage extends Component {
 			rep_name:""
                 };
 
-        //this.socket = io();
-	  this.socket = io('localhost:5000');
-
+	  //this.socket = io('localhost:5000');
+	  this.socket = io('https://webchatlabs10.herokuapp.com');
 
         this.socket.on(this.state.uid, function(message) {
                 console.log('Incoming message:', message);
