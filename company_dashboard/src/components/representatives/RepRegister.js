8d44f704a61e95785ad08a5005009556b1ce4b69
verydecent <wnjhwng@gmail.com>
2019-03-06 16:08:32 -0800

    Updates home button on repregister

--- company_dashboard/src/components/representatives/RepRegister.js
+++ company_dashboard/src/components/representatives/RepRegister.js
@@ -132,7 +132,11 @@ class RepSignUpFormBase extends Component {
             <div>
               <div className="register-top-bar">
                 <img src="https://i.ibb.co/Mpy1WhB/3029ba78-770c-49a3-aaa6-6a6cfc58b56c.png" alt="logo" />
-                <p>Send me back to the <Link to={ROUTES.LANDING}>home page</Link></p>
+                <Link to={ROUTES.LANDING}>
+                    <RaisedButton 
+                      label="Home"
+                    />
+                  </Link>
               </div>
                 <p className="header">Register an Account</p> 
             <form onSubmit={this.onSubmit}>
