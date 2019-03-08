0a0b4314ee65c042e882d3a8289f241ba6e865d2
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-03-07 21:41:58 -0800

    message table migrated

---
+++ data/migrations/20190307213344_create-messages-table.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('messages', table => {
+            table.increments('id').primary();       
+            table.integer('conversation_id')
+                .references('id')
+                .inTable('conversations');  
+            table.integer('author_id');    // Ideally this would be a FK but leaving it free for first iteration because author could be customer or rep; 
+            table.string('body')
+                .notNullable();  
+        })
+    ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfEsists('messages')
+    ])
+};
