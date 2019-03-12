a7878f0b7635ec7e1659aa0648accab8ee52c52f
chainchompa <chainchompa@gmail.com>
2019-03-12 18:55:58 -0400

    added navigation to billing page

--- company_dashboard/src/components/settings/Billing.js
+++ company_dashboard/src/components/settings/Billing.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import { Elements, StripeProvider } from "react-stripe-elements";
 import CheckoutForm from "./CheckoutForm";
+import "./Billing.css";
+import Navigation from "../Navigation";
 
 class Billing extends Component {
   render() {
     return (
-      <StripeProvider apiKey="pk_test_rY8prrYy1Hij91qrNdI5zpYu">
-        <div className="example">
-          <h1>Lifetime purchase of webchat for $30</h1>
-          <Elements>
-            <CheckoutForm />
-          </Elements>
-        </div>
-      </StripeProvider>
+      <div className="billing-container">
+        <Navigation />
+        <StripeProvider apiKey="pk_test_rY8prrYy1Hij91qrNdI5zpYu">
+          <div className="example">
+            <h1>Lifetime purchase of webchat for $30</h1>
+            <Elements>
+              <CheckoutForm />
+            </Elements>
+          </div>
+        </StripeProvider>
+      </div>
     );
   }
 }
