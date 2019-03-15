0e2a477ab654cc6f908a2039687a21ac8d053566
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-03-15 01:27:16 -0700

    Styling changes to Chat dashboard.

--- company_dashboard/src/components/ChatDashboard/ChatView.js
+++ company_dashboard/src/components/ChatDashboard/ChatView.js
@@ -23,16 +23,17 @@ const styles = theme => ({
     flexGrow: 1,
     overflow: 'hidden',
     padding: `0 ${theme.spacing.unit * 3}px`,
+    height: 500,
+    overflowY: 'scroll'
   },
   paper: {
     maxWidth: 400,
     margin: `${theme.spacing.unit*2}px auto`,
     padding: theme.spacing.unit * 2,
   },
-  root: {
-    height: 700,
-    overflowY: 'scroll'
-  },
+//   root: {
+//     overflowY: 'scroll'
+//   },
   form: {
     display: "flex",
     flexDirection: "column",
