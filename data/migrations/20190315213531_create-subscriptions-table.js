5954b3c13552cf08c602fc5f57da211d8472b61b
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-03-15 23:42:53 -0700

    comments added to subscriptions migration file.

--- data/migrations/20190315213531_create-subscriptions-table.js
+++ data/migrations/20190315213531_create-subscriptions-table.js
@@ -5,15 +5,15 @@ exports.up = function(knex, Promise) {
             table.integer('company_id')
                 .references('id')
                 .inTable('companies');  
-            table.string('stripe_customer_id')          
+            table.string('stripe_customer_id')          // Stripe-provided ID for Stripe customer account
                 .notNullable();
-            table.string('stripe_subscription_id')      
+            table.string('stripe_subscription_id')      // Stripe-provided ID for subscription, specific to customer account
                 .notNullable();
-            table.string('stripe_subscription_status')  
+            table.string('stripe_subscription_status')  // e.g., 'active'
                 .notNullable();    
-            table.string('stripe_plan_id')             
+            table.string('stripe_plan_id')              // Stripe-provided ID for Stripe payment plan
                 .notNullable();
-            table.string('stripe_plan_nickname')        
+            table.string('stripe_plan_nickname')        // e.g., 'Basic Monthly'
                 .notNullable();
         })
     ])
