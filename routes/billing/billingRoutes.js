dd481a7afaf1105ba60a67e2e9d8ad11388decb9
verydecent <wnjhwng@gmail.com>
2019-03-17 13:00:06 -0700

    pull

--- routes/billing/billingRoutes.js
+++ routes/billing/billingRoutes.js
@@ -1,33 +1,77 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../../data/helpers/companiesDb');
+const db = require('../../data/helpers/subDb');
+const repDb = require('../../data/helpers/repDb');
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
-router.post("/charge", async (req, res) => {
-    // Take the Stripe token and company_id from req.body:
-    let { token, company_id } = req.body;
-    // Get the company by id:
-    const company = db.getById(company_id);
+router.post('/addSub', async (req, res) => {
+    let plan = req.body.subscription.plan;  // comes from Billing form
+    let uid = req.body.uid;                  // rep uid attached to body in firebase auth check on base url
 
-    if (company.length === 0) {
-        console.log("Company not found");
-        return res.status(500).send("Company not found");
-    }
-    // Charge company $30:
-    try {
-        let { status } = await stripe.charges.create({
-            amount: 3000,
-            currency: "usd",
-            source: req.body.token
-        });
-        // Change has_paid boolean to true for company
-        await db.updatePayment(company_id, true);
-        res.json({ status });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: err.message });
+    let rep_info = await repDb.getByUid(uid);               // get info for current rep using uid
+    let company_id = rep_info.company_id;              // take company_id from rep info
+    let existingSub = await db.getSub(company_id);          // check Database for an existing subscription:
+
+
+
+    if (!existingSub) {
+      try {
+        let customer = await stripe.customers.create({
+          email: rep_info.email,     // ** attach to body
+          source: req.body.id,            // ** attach to body-- WHAT IS SOURCE?
+        })
+
+        let charge = await stripe.subscriptions.create({
+          customer: customer.id,    // comes from creatCustomer call above
+          items: [{ plan }],        
+        })
+
+        let subInfo = {
+          company_id: company_id,
+          stripe_customer_id: charge.customer,
+          stripe_subscription_id: charge.id,
+          stripe_subscription_status: charge.status,
+          stripe_plan_id: charge.plan.id,
+          stripe_plan_nickname: charge.plan.nickname,
+        }
+
+        let inserted = await db.insert(subInfo)
+
+        res.status(201).json({ message: `subscription created`, inserted })
+      } catch (err) {
+        res.status(500).json({ error: err })
+      }
+    } else {
+      res.status(400).json({ message: `you already have a subscription` })
     }
 });
+
+// router.post("/charge", async (req, res) => {
+//     // Take the Stripe token and company_id from req.body:
+//     let { token, company_id } = req.body;
+//     // Get the company by id:
+//     const company = db.getById(company_id);
+
+//     if (company.length === 0) {
+//         console.log("Company not found");
+//         return res.status(500).send("Company not found");
+//     }
+//     // Charge company $30:
+//     try {
+//         let { status } = await stripe.charges.create({
+//             amount: 3000,
+//             currency: "usd",
+//             source: req.body.token
+//         });
+//         // Change has_paid boolean to true for company
+//         await db.updatePayment(company_id, true);
+//         res.json({ status });
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json({ message: err.message });
+//     }
+// });
 
 module.exports = router;
