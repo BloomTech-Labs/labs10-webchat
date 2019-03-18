d2a8fd43fb294c725d5214de72feab40a12d0af9
Linda Yang <yanglinda24@gmail.com>
2019-03-18 16:34:28 -0400

    Removed commented codes

--- company_dashboard/src/components/DummyWebsite/DummyWebsite.js
+++ company_dashboard/src/components/DummyWebsite/DummyWebsite.js
@@ -28,7 +28,7 @@ const styles = theme => ({
 	toolBar: {
 		display: 'flex',
 		justifyContent: 'space-between',
-		height: 90,
+		height: 100,
 			[theme.breakpoints.down('sm')]: {
 				// backgroundColor: 'blue',
 			},
@@ -342,36 +342,3 @@ DummyWebsite.propTypes = {
 
 export default withStyles(styles)(DummyWebsite);
 
-// class DummyWebsite extends Component {
-//     render() {
-//         return (
-//             <div className="dummy-website">
-//                 <div className="dummy-top-info">
-//                     <h1>Joes Plumbing</h1>
-//                     <p>10800 Greene Street</p>
-//                     <p>1-800-456-7890</p>
-//                     <p>Monday-Saturday 9:00-5:00</p>
-//                 </div>
-//                 <div className="dummy-navigation-container">
-//                     <p className="navigation-link">HOME</p>
-//                     <p className="navigation-link">ABOUT</p>
-//                     <p className="navigation-link">SERVICES</p>
-//                     <p className="navigation-link">PRODUCTS</p>
-//                     <p className="navigation-link">BLOG</p>
-//                     <p className="navigation-link">GET APPOINTMENT</p>
-//                 </div>
-//                 <div className="dummy-info">
-//                     <h3>Joe's Plumbing</h3>
-//                     <ul>
-//                         <li>Upfront, Flat Rate Pricing</li>
-//                         <li>Never an Overtime Charge</li>
-//                         <li>Courtous, Uniformed Professionals</li>
-//                     </ul>
-// 		<Button className="webChatAppBtn">Chat!</Button> <iframe className="wcaIFRAME"></iframe>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// export default DummyWebsite;
