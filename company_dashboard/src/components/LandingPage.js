aa6a82a252bd53005e1cef75576c45b2abf24cd0
Sukhada Gholba <sukhadagholba@gmail.com>
2019-03-17 21:11:04 -0700

    Fixed a bug-Added missing constructor and super to react components to access this.props to access firebase methods

--- company_dashboard/src/components/LandingPage.js
+++ company_dashboard/src/components/LandingPage.js
@@ -296,7 +296,11 @@ const styles = theme => ({
 })
 
 class LandingPage extends Component {
-  render() {
+constructor(props){
+    super(props);
+}	
+	
+render() {
     const { classes } = this.props;
 
     return (
