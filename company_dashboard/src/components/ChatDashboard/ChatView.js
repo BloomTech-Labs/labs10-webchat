4f7d97430eefa059c46aca4d8f698344fdcab88c
verydecent <wnjhwng@gmail.com>
2019-03-18 14:38:29 -0700

    merge conflict

--- company_dashboard/src/components/ChatDashboard/ChatView.js
+++ company_dashboard/src/components/ChatDashboard/ChatView.js
@@ -48,78 +48,77 @@ import { ThemeProvider, MessageList, MessageGroup, MessageText, MessageTitle, Me
 
 
 class ChatView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rep_uid: null,
-            message: '',
-            messages: [],
-            is_closed: false,
-            image_id: null,
-            url: "",
-            rep_name: "",
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      rep_uid: null,
+      message: '',
+      messages: [],
+      is_closed: false,
+      image_id: null,
+      url: "",
+      rep_name: "",
+    };
 
-	    this.socket = io('localhost:5000');
-	    // this.socket = io('https://webchatlabs10.herokuapp.com');
+    this.socket = io('localhost:5000');
+    // this.socket = io('https://webchatlabs10.herokuapp.com');
 
-        this.socket.on(this.props.currentConvoSocket, function(message) {
-            console.log('Incoming message:', message);
-            addMessage(message);
+    this.socket.on(this.props.currentConvoSocket, function(message) {
+      console.log('Incoming message:', message);
+      addMessage(message);
+    });
+
+    const addMessage = (newMessage) => {
+      console.log("newMessage to add in ChatView: ", newMessage);
+      const newMessages = [];
+      this.state.messages.forEach(message => {
+        newMessages.push({...message});
+      });
+      newMessages.push(newMessage);
+      this.setState({ messages: newMessages });
+    }
+  }
+
+
+  componentDidMount() {
+    console.log('ChatView CDM state: ', this.state);
+    console.log('ChatView CDM props: ', this.props);
+
+    // Get details on the current rep:
+    const repRequest = axios.get("/api/reps/alldetails");
+    repRequest.then(rep => {
+      const id = this.props.currentConvoId;  // Get convo_id from props
+      const messageRequest = axios.get(`/api/chat/messages/${id}`);
+      messageRequest
+        .then(response => {
+          this.setState({
+            messages: response.data,
+            rep_uid: rep.data.uid,
+            image_id: rep.data.image_id,
+            url: rep.data.url,
+            rep_name: rep.data.name,
+          }, () => {
+            console.log('ChatView state after getting messages in CDU: ', this.state);
+          });
+        })
+        .catch(error => {
+          console.log(error.message);
+          //this.setState({error:error});
         });
-
-        const addMessage = (newMessage) => {
-            console.log("newMessage to add in ChatView: ", newMessage);
-            const newMessages = [];
-            this.state.messages.forEach(message => {
-                newMessages.push({...message});
-            });
-            newMessages.push(newMessage);
-            this.setState({ messages: newMessages });
-        }
-       
-    }
-
-
-    componentDidMount() {
-        console.log('ChatView CDM state: ', this.state);
-        console.log('ChatView CDM props: ', this.props);
-
-        // Get details on the current rep:
-        const repRequest = axios.get("/api/reps/alldetails");
-        repRequest.then(rep => {
-            const id = this.props.currentConvoId;  // Get convo_id from props
-            const messageRequest = axios.get(`/api/chat/messages/${id}`);
-            messageRequest
-                .then(response => {
-                    this.setState({
-                        messages: response.data,
-                        rep_uid: rep.data.uid,
-                        image_id: rep.data.image_id,
-                        url: rep.data.url,
-                        rep_name: rep.data.name,
-                    }, () => {
-                        console.log('ChatView state after getting messages in CDU: ', this.state);
-                    });
-                })
-                .catch(error => {
-                        console.log(error.message);
-                        //this.setState({error:error});
-                });
-            // this.setState({
-            //     rep_uid: rep.data.uid,
-            //     image_id: rep.data.image_id,
-            //     url: rep.data.url,
-            //     rep_name: rep.data.name,
-            // });
-            })
-            .catch(error => {
-            console.log(error.message);
-            //this.setState({error:error});
-            });
-        // Scroll to latest message whenever component mounts
-        // this.scrollToBottom();
-    }
+      // this.setState({
+      //     rep_uid: rep.data.uid,
+      //     image_id: rep.data.image_id,
+      //     url: rep.data.url,
+      //     rep_name: rep.data.name,
+      // });
+      })
+      .catch(error => {
+        console.log(error.message);
+        //this.setState({error:error});
+      });
+    // Scroll to latest message whenever component mounts
+    // this.scrollToBottom();
+  }
 
     componentWillReceiveProps(newProps) {
         console.log('ChatView CWRP props: ', newProps);
@@ -212,96 +211,96 @@ class ChatView extends Component {
         const conversation_summary = `${this.props.summary}`
         return(
 
-        <div className="chat-view">
+
+          <div className="chat-view">
             <ThemeProvider>
             <MuiThemeProvider>
+              <div className="chat-view-header">
+                  <MessageGroup>
+                      <MessageTitle>Serving Customer: {customer_name}</MessageTitle>
+                      <MessageText>{conversation_summary}</MessageText>
+                  </MessageGroup>
+              </div>
+              <div style={{ maxWidth: '100%', height: '100%' }}>
+                <div className="messageList" style={{ height: 500 }}>
+                  <MessageList>
+                    {this.state.messages.map((message, index) => {
+                      console.log(message);
+                      if(customer_name === message.author_name) {
+                        return (
+                          <Row reverse>
+                            <AgentBar>
+                                <img src={message.image_url} style={{ width: 55, height: 55 }}/>
+                            </AgentBar>
 
-                    <div className="chat-view-header">
-                        <MessageGroup>
-                            <MessageTitle>Serving Customer: {customer_name}</MessageTitle>
-                            <MessageText>{conversation_summary}</MessageText>
-                        </MessageGroup>
-                    </div>
-                <div style={{ maxWidth: '100%', height: '100%' }}>
-                    <div className="messageList" style={{ height: 500 }}>
-                        <MessageList>
-                                {this.state.messages.map((message, index) => {
-                                    console.log(message);
-                                    if(customer_name === message.author_name) {
-                                        return (
-                                            <Row reverse>
-                                                <AgentBar>
-                                                    <img src={message.image_url} style={{ width: 55, height: 55 }}/>
-                                                </AgentBar>
-
-                                                    <Message
-                                                        authorName={message.author_name}
-                                                        isOwn={true}
-                                                    >
-                                                        <MessageText>
-                                                            {message.body}
-                                                        </MessageText>
-                                                    </Message>
-                                            </Row>
-                                        );
-                                    } else {
-                                        return (
-                                            <Row>
-                                                <AgentBar>
-                                                    <img src={message.image_url} style={{ width: 55, height: 55 }}/>
-                                                </AgentBar>
-                                                <Message
-                                                    authorName={message.author_name}
-                                                >
-                                                    <MessageText>
-                                                        {message.body}
-                                                    </MessageText>
-                                                </Message>
-                                            </Row>
-                                        );
-                                    }
-                                })}
-                        </MessageList>
-                    </div>
-                    <form 
-                        className="form"    
-                        onSubmit={this.onSubmit}
-                    >
-                        <br/>
-                        <br/>
-                        <br/>
-                        <input
-                            hintText="message"
-                            name="message"
-                            type="text"
-                            style ={{width: '80%', padding:'0px 15px 0px 10px'}}
-                            inputStyle ={{width: '100%' }}
-                            className="messageInput"
-                            value={this.state.message}
-                            onChange={this.onChange}
-                        />
-                        <br/>
-                        <br/>
-                        
-                        {is_closed ? (
-                            <p>This conversation is closed.</p>
-                        ) : (
-                            <div className="footer-buttons">
-                            <RaisedButton
-                                label="send"
-                                primary={true}
-                                type="submit"
-                            />
-                            
-                            <RaisedButton
-                                label="End Conversation"
-                                secondary={true}
-                                onClick={this.handleCloseConvo}
-                            />
-                            </div>
-                        )}
-                    </form>
+                            <Message
+                                authorName={message.author_name}
+                                isOwn={true}
+                            >
+                                <MessageText>
+                                    {message.body}
+                                </MessageText>
+                            </Message>
+                          </Row>
+                        );
+                      } else {
+                        return (
+                          <Row>
+                            <AgentBar>
+                              <img src={message.image_url} style={{ width: 55, height: 55 }}/>
+                            </AgentBar>
+                            <Message
+                              authorName={message.author_name}
+                            >
+                              <MessageText>
+                                {message.body}
+                              </MessageText>
+                            </Message>
+                          </Row>
+                        );
+                        }
+                    })}
+                  </MessageList>
                 </div>
+                <form 
+                  className="form"    
+                  onSubmit={this.onSubmit}
+                >
+                  <br/>
+                  <br/>
+                  <br/>
+                  <input
+                      hintText="message"
+                      name="message"
+                      type="text"
+                      style ={{width: '80%', padding:'0px 15px 0px 10px'}}
+                      inputStyle ={{width: '100%' }}
+                      className="messageInput"
+                      value={this.state.message}
+                      onChange={this.onChange}
+                  />
+                  <br/>
+                  <br/>
+                    
+                  {is_closed ? (
+                      <p>This conversation is closed.</p>
+                  ) : (
+                      <div className="footer-buttons">
+                      <RaisedButton
+                          label="send"
+                          primary={true}
+                          type="submit"
+                      />
+                      
+                      <RaisedButton
+                          label="End Conversation"
+                          secondary={true}
+                          onClick={this.handleCloseConvo}
+                      />
+                      </div>
+                  )}
+                </form>
+              </div>
                         
 
                         {/* <div className="rootReplacement"> */}
@@ -316,7 +315,7 @@ class ChatView extends Component {
                         {/* </div> */}
             </MuiThemeProvider>
             </ThemeProvider>
-        </div>
+          </div>
     );
   }
 }
