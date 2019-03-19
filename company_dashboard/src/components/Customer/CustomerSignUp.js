fac67cd9742d063228e05d0efd57b5de2da5aaa1
chainchompa <chainchompa@gmail.com>
2019-03-19 15:55:12 -0400

    moved name to the top of customer register

--- company_dashboard/src/components/Customer/CustomerSignUp.js
+++ company_dashboard/src/components/Customer/CustomerSignUp.js
@@ -24,23 +24,23 @@ const CustomerSignUpPage = () => (
 class CustomerSignUpFormBase extends Component {
   constructor(props) {
     super(props);
-      
-     this.state = {     
+
+     this.state = {
     	email:"",
       password:"",
       password1:"",
-      company_id:2,     
+      company_id:2,
       name:"",
-      uid:"",     
-      summary:"",     
+      uid:"",
+      summary:"",
       error:null,
-      registered:false,     
+      registered:false,
     };
   }
 
  onSubmit = event => {
     const {email, password } = this.state;
-	
+
     if(this.state.password !== this.state.password1){
     	this.setState({error:{message:"Passwords don't match"}, password:"", password1:""});
     }
@@ -50,23 +50,23 @@ class CustomerSignUpFormBase extends Component {
       .doCreateUserWithEmailAndPassword(email, password)
       .then(authUser => {
             console.log(authUser.user.uid);
-		
+
 	this.props.firebase.auth.currentUser.getIdToken()
           .then(idToken => {
             console.log("idToken from curentUser: ", idToken);
             axios.defaults.headers.common['Authorization'] = idToken;
-		
-	    console.log('company is: ', this.state.company_id);		  
-	    const data ={company_id: this.state.company_id, name: this.state.name, email: this.state.email, summary: this.state.summary, uid:authUser.user.uid}	
-		
+
+	    console.log('company is: ', this.state.company_id);
+	    const data ={company_id: this.state.company_id, name: this.state.name, email: this.state.email, summary: this.state.summary, uid:authUser.user.uid}
+
 	    	//add customer details to customer table
 		const request = axios.post('/api/customers', data);
 
         	request.then(response => {
                 console.log('newly added customer', response.data);
                         //this.setState({allreps: r.data});
-			
-			
+
+
            		this.props.history.push({
                   	pathname: ROUTES.CHAT_PAGE,
                   	state: {
@@ -80,86 +80,86 @@ class CustomerSignUpFormBase extends Component {
                 .catch(error =>{
                         console.log(error.message);
                         this.setState({error:error});
-                })	  
-		
+                })
+
           })
           .catch(error => {
             this.setState({ error:error });
           })
-    
+
   });
-   } 
+   }
 	     event.preventDefault();
- }	 
+ }
 
 
   onChange = event => {
-	
+
 	this.setState({ [event.target.name]: event.target.value });
   };
 
 
 render() {
-    const {email, password, password1, error, name, summary} = this.state; 
+    const {email, password, password1, error, name, summary} = this.state;
     const condition = password === '' || password1 === '' || email === '' || name === '' || summary === '';
 
 
     return (
-      <div className="customer-signup"> 
-      	<MuiThemeProvider> 
-          {this.state.logged ? 
+      <div className="customer-signup">
+      	<MuiThemeProvider>
+          {this.state.logged ?
             (<Typography variant='display1' align='center' gutterBottom>
               Successfully Logged In</Typography>):(
             <div>
               <div className="customer-signup-top-bar">
                 <img src="https://i.ibb.co/Mpy1WhB/3029ba78-770c-49a3-aaa6-6a6cfc58b56c.png" alt="logo" />
               </div>
-            
+
 	            <form onSubmit={this.onSubmit}>
-	              <TextField
-                  style = {{width: '65%'}}
-                  hintText="Enter your Email"
-                  floatingLabelText="Email"
-                  name="email"
-                  type="text"   
-                  required={true}
-                  value={this.state.email}
-                  onChange={this.onChange}
-                />
-                <br/>       
-        
-                <TextField
-                  style = {{width: '65%'}}
-                  hintText="Enter your password"
-                  floatingLabelText="Password"
-                  required={true}
-                  name="password"
-                  type="password"  
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-                <br/>
-              
-                <TextField
-                  style = {{width: '65%'}}
-                  hintText="Re-enter your password"
-                  floatingLabelText="Re-enter password"
-                  name="password1"
-                  type="password"
-                  required={true}  
-                  value={this.state.password1}
-                  onChange={this.onChange}
-                />
-                <br/>
-        
                 <TextField
                   style = {{width: '65%'}}
                   hintText="Enter your name"
                   floatingLabelText="Name"
                   required={true}
                   name="name"
-                  type="text" 
+                  type="text"
                   value={this.state.name}
+                  onChange={this.onChange}
+                />
+                <br/>
+                
+	              <TextField
+                  style = {{width: '65%'}}
+                  hintText="Enter your Email"
+                  floatingLabelText="Email"
+                  name="email"
+                  type="text"
+                  required={true}
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+                <br/>
+
+                <TextField
+                  style = {{width: '65%'}}
+                  hintText="Enter your password"
+                  floatingLabelText="Password"
+                  required={true}
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+                <br/>
+
+                <TextField
+                  style = {{width: '65%'}}
+                  hintText="Re-enter your password"
+                  floatingLabelText="Re-enter password"
+                  name="password1"
+                  type="password"
+                  required={true}
+                  value={this.state.password1}
                   onChange={this.onChange}
                 />
                 <br/>
@@ -170,20 +170,20 @@ render() {
                   floatingLabelText="What can we help you with?"
                   required={true}
                   name="summary"
-                  type="text"   
+                  type="text"
                   value={this.state.summary}
                   onChange={this.onChange}
                 />
-                <br/>	
+                <br/>
 
                 <RaisedButton
-                    label="SignUp" 
-                    primary={true} 
+                    label="SignUp"
+                    primary={true}
                     type="submit"
-                    disabled={condition} 
-              />       
-              
-        {error && <p>{error.message}</p>}    
+                    disabled={condition}
+              />
+
+        {error && <p>{error.message}</p>}
             </form>
             </div>)}
         </MuiThemeProvider>
@@ -205,6 +205,3 @@ const CustomerSignUpForm = withRouter(withFirebase(CustomerSignUpFormBase));
 export default CustomerSignUpPage;
 
 export { CustomerSignUpForm, CustomerSignUpLink };
-
-
-
