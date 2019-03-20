24ed84579a0fcd54e6eb5cff4cfee70ae3ae964f
chainchompa <chainchompa@gmail.com>
2019-03-20 10:23:12 -0400

    changed className to class in code snippet in admin panel

--- company_dashboard/src/components/Admin/AdminPanel.js
+++ company_dashboard/src/components/Admin/AdminPanel.js
@@ -321,7 +321,7 @@ class AdminPanelBaseForm extends React.Component {
                 rowsMax={Infinity}
                 fullWidth
                 className={[classes.TextField, "code-snippet"].join(' ')}
-                value={"<Button className='webChatAppBtn'><img className='webChatAppBtnImg'/></Button> <iframe className='wcaIFRAME'></iframe><script src='https://labs10-webchat.netlify.com/snippet.js?company_id="+this.state.company_id+"'></script>"}
+                value={"<Button class='webChatAppBtn'><img class='webChatAppBtnImg'/></Button> <iframe class='wcaIFRAME'></iframe><script src='https://labs10-webchat.netlify.com/snippet.js?company_id="+this.state.company_id+"'></script>"}
                 margin='normal'
                 variant='outlined'
               />
