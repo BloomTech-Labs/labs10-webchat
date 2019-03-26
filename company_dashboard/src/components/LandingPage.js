42988ef25e908448fc07ae2d231a8161bd993699
Linda Yang <yanglinda24@gmail.com>
2019-03-26 19:04:08 -0400

    Changed sign up button to white color

--- company_dashboard/src/components/LandingPage.js
+++ company_dashboard/src/components/LandingPage.js
@@ -312,7 +312,7 @@ render() {
               <p className={classes.navButton}>SIGN IN</p>
             </a>
             <a href='/repregister'>
-              <p className={classes.signupNav}>SIGN UP</p>
+              <p className={classes.navButton}>SIGN UP</p>
             </a>
           </Toolbar>
         </AppBar>
